4367459f664e1cec1ab1f0a5642877a8deb1a430
wingyintang <wingyintangtwy@gmail.com>
2021-12-12 04:00:24 +0800

    Modify draw_grid_and_secs to show second and note number

--- src/chart_pic_generator.py
+++ src/chart_pic_generator.py
@@ -31,6 +31,7 @@ SEC_HEIGHT = 500
 X_MARGIN = 100
 Y_MARGIN = 70
 RIGHT_MARGIN = 0
+LEFT_MARGIN = 50
 MAX_Y = 5000
 MAX_SECS_PER_GROUP = (MAX_Y - Y_MARGIN * 2) // SEC_HEIGHT
 
@@ -221,7 +222,7 @@ class BaseChartPicGenerator(ABC):
 
     def initialize_ui(self):
         self.y_total = MAX_SECS_PER_GROUP * SEC_HEIGHT + 2 * Y_MARGIN
-        self.x_total = (2 * X_MARGIN + (self.lane_count - 1) * self.LANE_DISTANCE) * self.n_groups + RIGHT_MARGIN
+        self.x_total = LEFT_MARGIN + (2 * X_MARGIN + (self.lane_count - 1) * self.LANE_DISTANCE) * self.n_groups + RIGHT_MARGIN
 
         self.label = QLabel()
         self.label.setAlignment(Qt.AlignBottom)
@@ -240,7 +241,7 @@ class BaseChartPicGenerator(ABC):
         self.x_max = min(MAX_WINDOW_WIDTH, self.x_total + 20)
 
     def get_x(self, lane, group):
-        return X_MARGIN + lane * self.LANE_DISTANCE + (
+        return LEFT_MARGIN + X_MARGIN + lane * self.LANE_DISTANCE + (
                 2 * X_MARGIN + (self.lane_count - 1) * self.LANE_DISTANCE) * group
 
     def get_y(self, sec, group=None, offset_group=0):
@@ -377,8 +378,9 @@ class BaseChartPicGenerator(ABC):
                     self.p.setPen(horizontal_grid_light_pen)
                 y = self.get_y(sec, group=0)
                 self.p.drawLine(self.get_x(0, group), y, self.get_x(self.lane_count - 1, group), y)
-                self.p.drawText(QRect(self.get_x(0, group) - 111, y - 25, 70, 50), Qt.AlignRight,
-                                str(sec + MAX_SECS_PER_GROUP * group))
+                tm = sec + MAX_SECS_PER_GROUP * group
+                self.p.drawText(QRect(self.get_x(0, group) - 111, y - 25, 70, 100), Qt.AlignRight,
+                                "{}:{:0>2}\n{}".format(tm // 60, tm % 60, self.notes[self.notes['sec'] <= tm].shape[0]))
 
     @abstractmethod
     def draw_notes(self):
